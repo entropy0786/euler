6ce49739b939ff5be520f738e85c9fe0bc4dc8dc
Patrick Thomas <pthomas0786@gmail.com>
2018-10-20 18:04:09 -0500

    euler: problem 4 solution, cleaned. counts once but then will not go over any smaller numbers again.

--- problems/problem004/src/problem004/header.hpp
+++ problems/problem004/src/problem004/header.hpp
@@ -3,39 +3,126 @@
 
 #include <sstream>
 #include <array>
+#include <iostream>
 
-namespace details
+
+namespace euler
 {
+	//struct LargestPalendromeFunctor;
 
-    std::string revDigits(char* in)
-    {
-        //reverse digits.
-        return in;
-    }
+	namespace details
+	{
+		enum EAlgoTypes : int
+		{
+			eNaive = 0
+		};
+	}
 
-    std::string makePalindrome(char* in)
-    {
-        // input in
-    }
+	struct LargestPalendromeFunctor
+	{
+		LargestPalendromeFunctor(const int numDigits = 1)
+			: _numDigits(numDigits)
+		{
+			_startingNums.assign(makeMaxOfNdigits(numDigits));
+			std::cout << "finding largest Palendrome for " << numDigits << " digits, starting with ["
+				<< _startingNums[0] << ", " << _startingNums[1] << "]" << std::endl;
+		}
 
+		virtual ~LargestPalendromeFunctor()
+		{
+		}
 
+		template <details::EAlgoTypes AlgorithmT>
+		std::array<int, 2> getLargest();
 
-}
+		template<>
+		std::array<int, 2> getLargest<details::EAlgoTypes::eNaive>();
 
+	private:
+		/*
+		* @brief make N-digit max number
+		*/
+		int makeMaxOfNdigits(const int numDigits = 1)
+		{
+			std::stringstream ss;
+			for (int n = 0; n < numDigits; ++n)
+			{
+				ss << '9';
+			}
+			return std::stoi(ss.str());
+		}
 
-std::array<int, 2> largestPalendrome(const int numDigits)
-{
-    std::array<int, 2> arr;
-    std::stringstream ss;
-    std::string pal;
-    for (int n = 0; n < numDigits / 2; ++n)
-    {
-        // make palendrome.
-        ss << n;
-        pal = ss.str();
-        ss.str("");
+		int makeMinOfNdigits(const int numDigits = 1)
+		{
+			std::stringstream ss;
+			ss << '1';
+			for (int n = 1; n < numDigits; ++n)
+			{
+				ss << '0';
+			}
+			return std::stoi(ss.str());
+		}
 
-    }
+		bool isProdPalendrome(const int a, const int b) const
+		{
+			std::stringstream ss;
+			ss << (a*b);
+			std::string strProduct = ss.str();
+			auto itBegin = strProduct.begin();
+			auto itEnd = strProduct.end();
+			if (itBegin == itEnd)
+			{
+				return false;
+			}
+			--itEnd;
+
+			while (itEnd > itBegin)
+			{
+				if (*itBegin != *itEnd)
+					return false;
+				++itBegin;
+				--itEnd;
+			};
+			return true;
+		}
+
+	protected:
+		int _numDigits;
+		std::array<int, 2> _startingNums;
+	};
+
+	template<>
+	std::array<int, 2> LargestPalendromeFunctor::getLargest<details::EAlgoTypes::eNaive>()
+	{
+		std::array<int, 2> largestPalProduct;
+		largestPalProduct.assign(0);
+		std::size_t currentIndex = 0;
+		long highestProduct = -1;
+
+		std::size_t otherIndex = 1 - currentIndex;
+		int nMax0 = _startingNums[currentIndex];
+		int nMax1 = _startingNums[otherIndex];
+		int n0 = nMax0;
+		int nMin0 = makeMinOfNdigits(_numDigits);
+		while( n0 > 0)
+		{
+			for (int n1 = n0; n1 > largestPalProduct[1];)
+			{
+				if (isProdPalendrome(n0, n1) && n0*n1 > highestProduct)
+				{
+					long product = (long)n0 * (long)n1;
+					highestProduct = (product > highestProduct) ? product : highestProduct;
+					largestPalProduct[0] = n0;
+					largestPalProduct[1] = n1;
+				}
+				--n1;
+			}
+			--n0;
+		};
+
+		return largestPalProduct;
+	}
+
 }
 
 
--- problems/problem004/src/problem004/main.cpp
+++ problems/problem004/src/problem004/main.cpp
@@ -1,6 +1,7 @@
 /**
 Problem Statement:
-A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 
+2-digit numbers is 9009 = 91 × 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 **/
@@ -11,18 +12,18 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 #include <iostream>
 
 void main()
-{
-    std::vector<unsigned long> factors = { 3, 5 };
-    
+{    
     unsigned long numberBelow;
     std::cout << "pick a number to accrue all numbers below that are multiples of the input factors: ";
     std::cin >> numberBelow;
+
+	euler::LargestPalendromeFunctor functor(numberBelow);
     std::cout << "calculating...";
-
-    auto totalSum = getSumOfFactors(factors, numberBelow);
-    std::cout << "done!\n";
-    std::cout << "the sum is: " << totalSum << "\n";
-
+	{
+		auto totalSum = functor.getLargest<euler::details::EAlgoTypes::eNaive>();
+		std::cout << "done!\n";
+		std::cout << "the largest Palendrome is: " << totalSum[0] << " x " << totalSum[1] << " = " << (long)totalSum[0] * (long)totalSum[1] << "\n";
+	}
     std::cout << " \nPress any key to continue\n";
     std::cin.ignore();
     std::cin.get();
